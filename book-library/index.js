e40ba6e797aed441c7811176581c202fa48efd0c
Mstars_lab1_04 <mstars_lab1_04@Mstars-lab1-04s-iMac.local>
2022-04-05 11:30:58 +0800

    backendd

---
+++ book-library/index.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv').config();
+const express = require("express");
+const app = express();
+const port = 3001;
+const apiRoutes = require('./routes/api');
+const connection = require('./database')
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/', apiRoutes)
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    success: false,
+    message: `Can't find ${req.originalUrl} on this server`
+  })
+})
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
