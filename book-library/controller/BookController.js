0b1542e9d899929590ad3ce60708b3dbf05177a3
Mstars_lab1_04 <mstars_lab1_04@Mstars-lab1-04s-iMac.local>
2022-04-07 11:42:03 +0800

    controller

--- book-library/controller/BookController.js
+++ book-library/controller/BookController.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const Book = require('../models/Book');
-const { validationResult } = require('express-validator');
 
 const get_books = (req, res, next) => {
     Book.find({}, function(err, data) {
@@ -19,13 +18,6 @@ const get_books = (req, res, next) => {
 
 const add_book = (req, res, next) => {
     const data = req.body;
-    const errors = validationResult(req)
-    if(errors.isEmpty){
-        return res.status(400).json({
-            success: false,
-            errors: errors.errors
-        })
-    } else {
     Book.create(data, function (err, data) {
         if(err) res.json({
             success: false,
@@ -36,7 +28,7 @@ const add_book = (req, res, next) => {
             success: true,
             data: data
         })
-    })}
+    })
 }
 
 const delete_books = (req, res, next) => {
@@ -47,13 +39,6 @@ const delete_books = (req, res, next) => {
 
 const update_book = (req, res, next) => {
     const data = req.body;
-    const errors = validationResult(req)
-    if(errors.isEmpty){
-        return res.status(400).json({
-            success: false,
-            errors: errors.errors
-        })
-    } else {
     Book.updateOne({isbn: req.params.isbn}, data, function (err, data) {
         if(err) res.json({
             success: false,
@@ -64,7 +49,7 @@ const update_book = (req, res, next) => {
             success: true,
             data: data,
         });
-    })}
+    })
 }
 
 module.exports = {
--- book-library/routes/api.js
+++ book-library/routes/api.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const BookController = require('../controller/BookController')
-const BookAuthenticator = require('../middleware/create_update')
 
 // Requests
 router.get('/books', BookController.get_books);
-router.post('/addBook', BookAuthenticator.addBook(), BookController.add_book);
+router.post('/addBook', BookController.add_book);
 router.post('/deleteBook/:isbn', BookController.delete_books);
-router.post('/updateBook/:isbn', BookAuthenticator.addBook(), BookController.update_book);
+router.post('/updateBook/:isbn', BookController.update_book);
 
 module.exports = router;
